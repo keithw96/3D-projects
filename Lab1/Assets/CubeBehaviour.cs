1adf3bc999e2c0da3af6965be842da03333b8aa2
Keith Wilson <C00205321@itcarlow.ie>
2017-09-27 11:28:36 +0100

    Lab 1 - Roll a ball

---
+++ Lab1/Assets/CubeBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour {
+    public int rotatonalSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(rotatonalSpeed, rotatonalSpeed, rotatonalSpeed) * Time.deltaTime);
+	}
+}
