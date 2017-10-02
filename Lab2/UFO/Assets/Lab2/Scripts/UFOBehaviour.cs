8f23ad4eaee0deeba0a6fc96585622fdb4433704
Keith Wilson <C00205321@itcarlow.ie>
2017-10-02 16:48:15 +0100

    UFO tutorial almost complete

---
+++ Lab2/UFO/Assets/Lab2/Scripts/UFOBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOBehaviour : MonoBehaviour {
+    private Rigidbody2D rd2d;
+    float speed;
+
+	// Use this for initialization
+	void Start () {
+        rd2d = GetComponent<Rigidbody2D>();
+        speed = 2;
+	}
+	
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal")* speed;
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical) * speed;
+        rd2d.AddForce(movement);
+       
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+    }
+}
---
+++ Lab2/UFO/Assets/Lab2/Scripts/cameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	//moves the camera as if it is a child of player
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
