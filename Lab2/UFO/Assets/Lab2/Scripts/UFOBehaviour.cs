1c5b9f1bf135aa047c4e3ee603a1acf1490d6ab3
Keith Wilson <C00205321@itcarlow.ie>
2017-10-04 15:06:20 +0100

    UFO complete osmosis started

--- Lab2/UFO/Assets/Lab2/Scripts/UFOBehaviour.cs
+++ Lab2/UFO/Assets/Lab2/Scripts/UFOBehaviour.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UFOBehaviour : MonoBehaviour {
     private Rigidbody2D rd2d;
     float speed;
-
-	// Use this for initialization
-	void Start () {
+    private int count;
+    public Text countText;
+    public Text winText;
+    // Use this for initialization
+    void Start () {
         rd2d = GetComponent<Rigidbody2D>();
         speed = 2;
-	}
+        count = 0;
+        setCountText();
+
+            }
 	
     void FixedUpdate()
     {
@@ -18,7 +24,6 @@ public class UFOBehaviour : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical) * speed;
         rd2d.AddForce(movement);
-       
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -26,11 +31,28 @@ public class UFOBehaviour : MonoBehaviour {
         if(other.gameObject.CompareTag("Pickup"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            setCountText();
+            
         }
     }
 
 	// Update is called once per frame
 	void Update () {
         transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+        if (count >= 9)
+        {
+            winText.text = "You Win";
+        }
+        else
+        {
+            winText.text = "";
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "count: " + count.ToString();
+
     }
 }
