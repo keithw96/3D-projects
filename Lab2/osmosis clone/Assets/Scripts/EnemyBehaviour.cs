e5bbed19742c1fd96a41eb2694e4e96ea2f5e82b
Keithw96 <c00203521@itcarlow.ie>
2017-10-09 01:33:25 +0100

    random movement implemented, collisions started but not complete

---
+++ Lab2/osmosis clone/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+    float scale;
+    Vector3 velocity;
+	// Use this for initialization
+	void Start () {
+        scale = (Random.Range(0.2f, 1.0f));
+        transform.localScale = new Vector3(scale, scale, scale);
+        velocity = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5));
+	}
+    // Update is called once per frame
+    void Update () {
+        transform.position += velocity * Time.deltaTime;
+	}
+}
---
+++ Lab2/osmosis clone/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+    private Rigidbody2D rd2d;
+    float speed;
+    // Use this for initialization
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        speed = 5;
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical) * speed;
+        rd2d.AddForce(movement);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Rigidbody2D otherRb2d = other.GetComponent<Rigidbody2D>();
+
+        if (other.gameObject.CompareTag("Enemies"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+       
+    }
+}
