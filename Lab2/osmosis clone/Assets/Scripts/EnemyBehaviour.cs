e717e055a8cc65912757bb4fc97a7bbc59b4999b
Keithw96 <c00203521@itcarlow.ie>
2017-10-09 10:28:09 +0100

    lab 2 complete

--- Lab2/osmosis clone/Assets/Scripts/EnemyBehaviour.cs
+++ Lab2/osmosis clone/Assets/Scripts/EnemyBehaviour.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class EnemyBehaviour : MonoBehaviour {
+    Rigidbody2D body;
     float scale;
     Vector3 velocity;
+    float startingMass;
 	// Use this for initialization
 	void Start () {
-        scale = (Random.Range(0.2f, 1.0f));
+        body = GetComponent<Rigidbody2D>();
+        startingMass = body.mass;
+        scale = (UnityEngine.Random.Range(0.2f, 2.0f));
         transform.localScale = new Vector3(scale, scale, scale);
-        velocity = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5));
+        velocity = new Vector3(UnityEngine.Random.Range(-5, 5), UnityEngine.Random.Range(-5, 5));
+        body.mass = scale;
 	}
     // Update is called once per frame
-    void Update () {
-        transform.position += velocity * Time.deltaTime;
+    void LateUpdate () {
+        body.AddRelativeForce(velocity);
 	}
 }
--- Lab2/osmosis clone/Assets/Scripts/PlayerBehaviour.cs
+++ Lab2/osmosis clone/Assets/Scripts/PlayerBehaviour.cs
@@ -1,38 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerBehaviour : MonoBehaviour {
     private Rigidbody2D rd2d;
     float speed;
-    // Use this for initialization
+    float startingMass;
+    int count = 0;
+    Vector2 movement;
+    Vector2 previousMovement;
+    public Text countText;
+    public Text gameOverText;   
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
+        startingMass = rd2d.mass;
         speed = 5;
-
+        SetCountText();
     }
 
     void FixedUpdate()
     {
+        previousMovement = movement;
         float moveHorizontal = Input.GetAxis("Horizontal") * speed;
         float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical) * speed;
+        movement = new Vector2(moveHorizontal, moveVertical) * speed;
         rd2d.AddForce(movement);
-    }
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        Rigidbody2D otherRb2d = other.GetComponent<Rigidbody2D>();
-
-        if (other.gameObject.CompareTag("Enemies"))
+        if (movement.magnitude < previousMovement.magnitude && rd2d.mass > 0.3f)
         {
-            other.gameObject.SetActive(false);
+            rd2d.mass -= (0.001f * rd2d.mass);
         }
     }
 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemies"))
+        {
+            if (rd2d.mass > other.rigidbody.mass)
+            {
+                count++;
+                other.gameObject.SetActive(false);
+                rd2d.mass += 0.3f;
+                SetCountText();
+            }
+            else
+            {
+                rd2d.gameObject.SetActive(false);
+                gameOverText.text = "You Lose";
+            }
+        }
+    }
     private void Update()
     {
-       
+        transform.localScale = new Vector3(rd2d.mass, rd2d.mass, rd2d.mass);
+        if(count >= 20)
+        {
+            gameOverText.text = "You Win";
+        }
+        else
+        {
+            gameOverText.text = "";
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Enemies Killed:" + count.ToString();
     }
 }
