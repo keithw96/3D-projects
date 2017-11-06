fdf0cf5c6bd9bca679c2a783838cddb2d565215f
Keithw96 <c00203521@itcarlow.ie>
2017-11-06 00:27:10 +0000

    Lab 4 complete

---
+++ Lab4/Particals tutorial/Assets/Scripts/rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour {
+    Rigidbody bd;
+	// Use this for initialization
+	void Start () {
+        bd = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bd.transform.Rotate(0.1f, 0.1f, 0);
+	}
+}
